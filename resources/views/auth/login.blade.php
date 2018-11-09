172c4b766901816512f0aff555e315396e19202b
adzannugraha <adzan.nugraha@gmail.com>
2018-11-09 18:14:36 +0700

    change url to route

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,7 +36,7 @@
   <div class="login-box-body">
     <p class="login-box-msg">Sign in to start your session</p>
 
-    <form action="{{ url('login') }}" method="post">
+    <form action="{{ route('login') }}" method="post">
       @csrf
       <div class="form-group has-feedback">
         <input type="email" class="form-control" placeholder="Email" name="email">
